e3d34512d5f6879682797b2e4f6ebd96dcb2a88e
zemanosek <pavlicekzemanek@seznam.cz>
2022-03-29 13:24:34 +0200

    Přidejte soubory projektu.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,16 @@
+﻿string cislo = Console.ReadLine();
+int a = Convert.ToInt32(cislo);
+
+string cislo2 = Console.ReadLine();
+int b = Convert.ToInt32(cislo2);
+
+if (a > b)
+{
+    Console.WriteLine(cislo);
+    Console.WriteLine(cislo2);
+}
+else
+{
+    Console.WriteLine(cislo2);
+    Console.WriteLine(cislo);
+}
