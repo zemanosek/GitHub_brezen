ccb0843dc330cc43a9f526fdf8b337ae06913f67
zemanosek <pavlicekzemanek@seznam.cz>
2022-03-29 13:26:18 +0200

    přidáno "Zadejte číslo..."

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,6 +1,8 @@
-﻿string cislo = Console.ReadLine();
+﻿Console.WriteLine("Zadejte 1. cislo");
+string cislo = Console.ReadLine();
 int a = Convert.ToInt32(cislo);
 
+Console.WriteLine("Zadejte 2. cislo");
 string cislo2 = Console.ReadLine();
 int b = Convert.ToInt32(cislo2);
 
