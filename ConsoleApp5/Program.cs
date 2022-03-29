0a63fdf6a94829a83e316082f6efce17e35aa95e
zemanosek <pavlicekzemanek@seznam.cz>
2022-03-29 13:31:04 +0200

    přidáná možnost "čísla se rovnají"

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -6,12 +6,16 @@ Console.WriteLine("Zadejte 2. cislo");
 string cislo2 = Console.ReadLine();
 int b = Convert.ToInt32(cislo2);
 
+if (a == b)
+{
+    Console.WriteLine("Cisla se rovnají");
+}
 if (a > b)
 {
     Console.WriteLine(cislo);
     Console.WriteLine(cislo2);
 }
-else
+if (a < b) 
 {
     Console.WriteLine(cislo2);
     Console.WriteLine(cislo);
